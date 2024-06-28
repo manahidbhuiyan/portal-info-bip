c877740f5bbff06161442fbed42ad9fff818f76e
Mahabub Alam Bhuiyan <ma.nahid960@gmail.com>
2024-06-28 22:43:54 +0600

    some modify on sign up

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-// JavaScript to add box shadow on scroll
-const container = document.getElementById('scroll-container');
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('scroll-container');
 
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 20) {
-    container.classList.add('box-shadow-on-scroll');
-  } else {
-    container.classList.remove('box-shadow-on-scroll');
-  }
+  window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+          container.classList.add('box-shadow-on-scroll');
+          console.log("Box shadow added");
+      } else {
+          container.classList.remove('box-shadow-on-scroll');
+          console.log("Box shadow removed");
+      }
+  });
 });
 
-
+// dropdown rotate
 document.addEventListener('DOMContentLoaded', function () {
   var dropdownButton = document.getElementById('dropdownMenuButton');
   var dropdownIcon = document.getElementById('dropdownIcon');
@@ -19,23 +22,3 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-//   document.getElementById('countrySelect').addEventListener('change', function() {
-//     const selectedOption = this.options[this.selectedIndex];
-//     const countryName = selectedOption.text;
-//     const countryFlag = selectedOption.getAttribute('data-flag');
-
-//     const flagElement = document.getElementById('countryFlag');
-//     const nameElement = document.getElementById('countryName');
-//     const displayElement = document.getElementById('countryDisplay');
-
-//     if (countryFlag) {
-//         flagElement.src = countryFlag;
-//         displayElement.style.display = 'flex';
-//     } else {
-//         displayElement.style.display = 'none';
-//     }
-
-//     nameElement.textContent = countryName;
-// });
-});
